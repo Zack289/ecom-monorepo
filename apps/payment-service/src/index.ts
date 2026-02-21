48fa27d4dd0a0c56b76aa2926cfe9755ebd0977d
Zack289 <ozack911@gmail.com>
2026-02-21 22:12:48 +0545

    Added the required backend web framework for the diff services

---
+++ apps/payment-service/src/index.ts
@@ -0,0 +1,35 @@
+import { serve } from "@hono/node-server";
+import { timeStamp } from "console";
+import { Hono } from "hono";
+import { uptime } from "process";
+
+const app = new Hono();
+
+app.get("/health", (c) => {
+  return c.json({
+    status: "ok",
+    uptime:process.uptime(),
+    timeStamp:Date.now(),
+  })
+});
+
+const start = async () => {
+  try {
+    serve(
+      {
+        fetch: app.fetch,
+        port: 8002,
+      },
+      (info) => {
+        console.log(
+          `"Order service is running on http://localhost:${info.port}`,
+        );
+      },
+    );
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start();
--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,12 +1,23 @@
-import express from 'express';
-import cors from 'cors'
+import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 
-app.use(cors({
-    origin: ["http://localhost:3002", "http://localhost:3003"], credentials: true
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:3002", "http://localhost:3003"],
+    credentials: true,
+  }),
+);
 
-app.listen(8000,()=>{
-    console.log("Product service is running on port http://localhost:8000")
-})
+app.get("/health", (req: Request, res: Response) => {
+  return res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    timeStamp: Date.now(),
+  });
+});
+
+app.listen(8000, () => {
+  console.log("Product service is running on port http://localhost:8000");
+});

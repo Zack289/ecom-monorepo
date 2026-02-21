34ef5b21a7fd128ce148aa8d0ced8a08ccc407d7
Zack289 <ozack911@gmail.com>
2026-02-21 23:33:53 +0545

    Tested the user token by connecting the client and the product service

--- apps/product-service/src/index.ts
+++ apps/product-service/src/index.ts
@@ -1,7 +1,12 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import "dotenv/config";
+
+import { clerkMiddleware, getAuth } from '@clerk/express'
+
 
 const app = express();
+
 
 app.use(
   cors({
@@ -10,6 +15,9 @@ app.use(
   }),
 );
 
+app.use(clerkMiddleware());
+
+
 app.get("/health", (req: Request, res: Response) => {
   return res.status(200).json({
     status: "ok",
@@ -17,6 +25,21 @@ app.get("/health", (req: Request, res: Response) => {
     timeStamp: Date.now(),
   });
 });
+
+app.get("/test", (req, res) => {
+   const auth = getAuth(req);
+   console.log(auth)
+   const userId = auth.userId;
+
+   if(!userId){
+    return res.status(401).json({ message: "You are not logged in"});
+   }
+  res.json({ message: "Product service authenticated" });
+});
+
+// app.get("/test", (req, res) => {
+//   res.json({ message: "Backend working" });
+// });
 
 app.listen(8000, () => {
   console.log("Product service is running on port http://localhost:8000");

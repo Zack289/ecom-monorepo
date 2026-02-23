658edb600a3b6318cd20f79438671fa4ad0a5a28
Zack289 <ozack911@gmail.com>
2026-02-23 22:49:29 +0545

    Added the product and category controller functions

--- apps/product-service/src/controllers/category.controller.ts
+++ apps/product-service/src/controllers/category.controller.ts
@@ -1,6 +1,7 @@
 import { prisma, Prisma } from "@repo/product-db";
 import { Request, Response } from "express";
 
+// Create a category
 export const createCategory = async (req: Request, res: Response) => {
   const data: Prisma.CategoryCreateInput = req.body;
 
@@ -9,8 +10,36 @@ export const createCategory = async (req: Request, res: Response) => {
   console.log("req.body:", req.body);
 };
 
+// Update Category
+export const updateCategory = async (req: Request, res: Response) => {
+  const { id } = req.params;
 
-export const updateCategory = async (req: Request, res: Response) => {};
-export const deleteCategory = async (req: Request, res: Response) => {};
-export const getCategories = async (req: Request, res: Response) => {};
+  const data: Prisma.CategoryUpdateInput = req.body;
 
+  const updatedCategory = await prisma.category.update({
+    where: { id: Number(id) },
+    data,
+  });
+
+  res.status(200).json(updatedCategory);
+};
+
+// Delete category
+export const deleteCategory = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const deletedCategory = await prisma.category.delete({
+    where: { id: Number(id) },
+  });
+
+  res
+    .status(200)
+    .json({ message: "Category deleted sucessfully", deletedCategory });
+};
+
+// Get all categories
+export const getCategories = async (req: Request, res: Response) => {
+  const categories = await prisma.category.findMany();
+
+  res.status(200).json(categories);
+};
